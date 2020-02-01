cdf8ce3fcd51269e9e6e8232ae096d3dba242c83
kasper023 <askar.baygenzhin@gmail.com>
2020-02-01 23:39:20 +0600

    first commit 1

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator1
+//
+//  Created by Сабина on 2/1/20.
+//  Copyright © 2020 Сабина. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
